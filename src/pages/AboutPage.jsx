ad4877d223cada49d1a7eab6b415345ef9c1ff0a
Siddhant <siddhantrsen73938@gmail.com>
2023-08-10 02:32:34 +0530

    About , Checkout page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,7 +4,20 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return <main>
+    <PageHero title='About' />
+    <Wrapper className='page section section-center'>
+      <img src={aboutImg} alt="Nice Interior" />
+      <article>
+        <div className="title">
+          <h2>Our Journey</h2>
+          <div className="underline"></div>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum sunt nisi nesciunt, officiis, voluptates, illum ab delectus pariatur hic quibusdam beatae nemo itaque incidunt dolores porro provident unde placeat soluta!lorem90
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit voluptatem, dolor molestias eligendi sed suscipit maiores ab. Fugit molestias aut veritatis iure accusantium nemo dolorem odio deserunt sunt! Corrupti assumenda rerum aut eum debitis atque quisquam eius laborum harum molestiae illo natus, at nostrum. Magni explicabo repellat sapiente adipisci, repellendus delectus maiores minima hic aut nostrum fugiat voluptate provident iste sit ratione, animi itaque cumque maxime ipsa ex nam? Ducimus incidunt aspernatur numquam. Assumenda nihil, et nam quidem quis corrupti in. Repudiandae ducimus nostrum, deleniti voluptatibus magnam ea maxime quo.</p>
+        </div>
+      </article>
+    </Wrapper>
+  </main>
 }
 
 const Wrapper = styled.section`
