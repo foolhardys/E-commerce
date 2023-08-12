0a86a5c7944b7174fa155621eb46008dc0cdea6d
Siddhant <siddhantrsen73938@gmail.com>
2023-08-12 15:13:01 +0530

    Single product page setup

--- src/context/products_context.jsx
+++ src/context/products_context.jsx
@@ -13,12 +13,17 @@ import {
   GET_SINGLE_PRODUCT_ERROR,
 } from '../actions'
 
+
+
 const initialState = {
   isSidebarOpen: false,
   products_loading: false,
   products_error: false,
   products: [],
   featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {},
 }
 
 const ProductsContext = createContext()
@@ -40,12 +45,22 @@ export const ProductsProvider = ({ children }) => {
     try {
       const response = await axios.get(url)
       const products = response.data
-      dispatch({ type: GET_PRODUCTS_SUCCESS , payload: products })
-      console.log(products);
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products })
     } catch (error) {
       dispatch({ type: GET_PRODUCTS_ERROR })
     }
 
+  }
+
+  const FetchSingleProduct = async (url) => {
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN })
+    try {
+      const response = await axios.get(url)
+      const singleProduct = response.data
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct })
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR })
+    }
   }
 
   useEffect(() => {
@@ -53,9 +68,8 @@ export const ProductsProvider = ({ children }) => {
   }, [])
 
 
-
   return (
-    <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
+    <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar,  FetchSingleProduct }}>
       {children}
     </ProductsContext.Provider>
   )
--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import { useParams, useHistory } from 'react-router-dom'
 import { useProductsContext } from '../context/products_context'
 import { single_product_url as url } from '../utils/constants'
-import { formatPrice } from '../utils/helpers'
 import {
   Loading,
   Error,
@@ -15,7 +14,60 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const SingleProductPage = () => {
-  return <h4>single product page</h4>
+
+  const { id } = useParams()
+  const history = useHistory()
+  const { single_product_loading: loading, single_product_error: error, single_product: product, FetchSingleProduct } = useProductsContext()
+
+
+  useEffect(() => {
+    FetchSingleProduct(`${url}${id}`)
+  }, [id])
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        history.push('/')
+      }, 3000)
+    }
+  }, [error])
+
+
+  if (loading) {
+    return <Loading />
+  }
+
+  if (error) {
+    return <Error />
+  }
+
+  const { name, description, stock, price, stars, reviews, company, images } = product
+
+  return <Wrapper>
+    <PageHero title={name} product />
+    <div className="section section-center page">
+      <Link to='/products' className='btn'>Back to products</Link>
+      <div className="product-center">
+        <ProductImages images={images} />
+        <section className="content">
+          <h2>{name}</h2>
+          <Stars stars={stars} reviews={reviews} />
+          <h5 className="price">₹{price}</h5>
+          <p className='description'> {description} </p>
+          <p className="info">
+            <span>Available :</span>
+            {stock > 0 ? 'In Stock' : 'Out of Stock'}
+          </p>
+          <p className="info">
+            <span>Brand :</span>
+            {company}
+          </p>
+          <hr />
+          {stock > 0 && <AddToCart product={product} />}
+        </section>
+      </div>
+    </div>
+  </Wrapper>
 }
 
 const Wrapper = styled.main`
